1de568ee34242625582034049933d76dc8b43dae
MariamHossam01 <81758943+MariamHossam01@users.noreply.github.com>
2021-07-24 17:31:57 +0200

    Update Bonus Problem.java

--- Session 1/Solutions/src/Bonus Problem.java
+++ Session 1/Solutions/src/Bonus Problem.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
 public class Problem4 {
-    public static void main(String [] args){
-        // Write your code here
-    }
+
+public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner myObj = new Scanner(System.in);
+		
+		String name =myObj.nextLine();
+		int x= name.length();
+     	Decode(x,name);
+
+		
+		myObj.close();
+	}
+	
+	public static void Decode(int l, String name)
+	{ 
+		
+		char[] charLetters = name.toCharArray();
+		char[] resultName = new char[name.length()];
+		
+		int x=0;
+
+		for(int i=l; i >0; i--) 
+	         { int z=i/2;
+	            if(i%2==0)
+	            {
+	            	resultName[z-1]= charLetters[x] ;
+	            }
+	            else	
+	            {
+	            	resultName[z+x]=charLetters[x] ;	
+	            }
+	            x++;
+	         }
+		
+		//System.out.print(Arrays.toString(charLetters)+" "+Arrays.toString(resultName)+"   ");
+		  for (int i = 0; i < l; i++) {
+	            System.out.print(resultName[i] );
+	        }
+	}
 }
