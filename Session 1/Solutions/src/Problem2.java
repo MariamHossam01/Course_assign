fffa04b64042929ce4730ffb5fac2bdca60571cc
MariamHossam01 <81758943+MariamHossam01@users.noreply.github.com>
2021-07-24 17:21:54 +0200

    Update Problem2.java

--- Session 1/Solutions/src/Problem2.java
+++ Session 1/Solutions/src/Problem2.java
@@ -1,5 +1,18 @@
 public class Problem2 {
-    public static void main(String [] args){
-        // Write your code here
-    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="java"; int i1=100;
+		String s2="cpp";  int i2=65;
+		String s3="python"; int i3=50;
+		  for (int j = 0; j < 20; j++) {
+		        System.out.print("=");
+		    }
+		System.out.printf("%n%-15s%03d %n%-15s%03d %n%-15s%03d %n",s1,i1, s2, i2,s3,i3);
+		  for (int j = 0; j < 20; j++) {
+		        System.out.print("=");
+		    }
+
+	}
+
 }
